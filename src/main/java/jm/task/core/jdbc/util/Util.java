19e41a2378dfdcd53525e3f158239655ffdb53b0
yojin <kirill_kondrashin99@mail.ru>
2021-10-12 22:41:53 +0400

    Commit Message

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+import java.sql.DriverManager;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String rootName = "root";
+    private static final String password = "@Kfcnjxrf29421";
+    private static final String connectionURL = "jdbc:mysql://localhost:3306/usertest";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(connectionURL, rootName, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
